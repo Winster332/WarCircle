39c4519a3320a2528d9148e532f14bc9b407afd0
Stas Martynov <stas-martynov-98@mail.ru>
2016-11-23 01:21:31 +0300

    Добавлены компоненты моделей

---
+++ GameEngine/UI/UIText.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine.UI
+{
+	public class UIText : BaseUI
+	{
+		public override void Dispose()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void Draw()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void Step(float dt)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
