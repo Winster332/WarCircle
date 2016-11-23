662f892cec28d506d08737f11c1a972ce52df25e
Stas Martynov <stas-martynov-98@mail.ru>
2016-11-23 18:59:23 +0300

    Создан UIText для вывода текстовой информации

--- GameEngine/UI/UIText.cs
+++ GameEngine/UI/UIText.cs
@@ -2,25 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
 
 namespace GameEngine.UI
 {
 	public class UIText : BaseUI
 	{
+		public event EventHandler ClickEnter;
+		public string Text { get; set; }
+		public Color ForeColor { get; set; }
+		public System.Drawing.Font Font { get; set; }
+		private System.Drawing.StringFormat sf;
+		public UIText()
+		{
+			Font = new System.Drawing.Font("Calibri", 14);
+			Width = 250;
+			Height = 40;
+			Text = "TEXT";
+			ForeColor = Color.FromArgb(50, 50, 50);
+			sf = new System.Drawing.StringFormat();
+			sf.Alignment = System.Drawing.StringAlignment.Center;
+			sf.LineAlignment = System.Drawing.StringAlignment.Center;
+		}
 		public override void Dispose()
 		{
-			throw new NotImplementedException();
 		}
 
 		public override void Draw()
 		{
-			throw new NotImplementedException();
+			Game.GetInstance().GetGraphics().Get().DrawString(Text, Font, new System.Drawing.SolidBrush(ForeColor), X, Y, sf);
 		}
 
 		public override void Step(float dt)
 		{
-			throw new NotImplementedException();
 		}
 	}
 }
